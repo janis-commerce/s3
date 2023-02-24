c3e05646b38c7d831ab9b25f7336732decafc0df
Manuel Vilche <manuel@fizzmod.com>
2023-02-24 15:07:06 -0300

    improve getObjectStream tests

--- tests/s3-test.js
+++ tests/s3-test.js
@@ -3,11 +3,11 @@
 const assert = require('assert');
 const sinon = require('sinon');
 
-// const { PassThrough, Readable } = require('stream');
-
+const { PassThrough, Readable } = require('stream');
 const { mockClient } = require('aws-sdk-client-mock');
-// const s3RequestSigner = require('@aws-sdk/s3-request-presigner');
+const { CreateMultipartUploadCommand, UploadPartCommand } = require('@aws-sdk/client-s3');
 
+const S3 = require('../lib/s3');
 const s3Wrapper = require('../lib/s3Wrapper');
 
 const {
@@ -20,13 +20,13 @@ const {
 	ListBucketsCommand,
 	CreateBucketCommand,
 	DeleteBucketCommand,
-	HeadObjectCommand
-	// CopyObjectCommand
+	HeadObjectCommand,
+	CopyObjectCommand
 } = s3Wrapper;
 
-const S3 = require('../lib/s3');
-// const GetObjectStream = require('../lib/getObjectStream');
-// const MyGetObjectStream = require('./getObjectTestClass');
+const GetObjectStream = require('../lib/getObjectStream');
+const MyGetObjectStream = require('./getObjectTestClass');
+
 const s3Params = {
 	Body: '<Binary String>',
 	Bucket: 'examplebucket',
@@ -35,19 +35,13 @@ const s3Params = {
 
 const message = 'random message error';
 
-describe('S3', () => {
+describe('S3 methods', () => {
 
 	beforeEach(() => {
 		this.s3ClientMock = mockClient(S3Client);
-		process.env.IS_OFFLINE = true;
-		process.env.S3_LOCAL_ENDPOINT = 'janis.localhost';
 	});
 
-	afterEach(() => {
-		this.s3ClientMock.restore();
-		delete process.env.IS_OFFLINE;
-		delete process.env.S3_LOCAL_ENDPOINT;
-	});
+	afterEach(() => this.s3ClientMock.reset());
 
 	context('getObject', () => {
 
@@ -328,206 +322,42 @@ describe('S3', () => {
 		});
 	});
 
-	// context('copyObject', () => {
+	context('copyObject', () => {
 
-	// 	const response = {
-	// 		LastModified: '2019-11-27T18:50:40.000Z',
-	// 		ETag: '"e77f5136cc15419e4c81beba285d4bde"'
-	// 	};
+		const response = {
+			LastModified: '2019-11-27T18:50:40.000Z',
+			ETag: '"e77f5136cc15419e4c81beba285d4bde"'
+		};
 
-	// 	it('Should return a the same response when calling to copyObject method', async () => {
+		it('Should return a the same response when calling to copyObject method', async () => {
 
-	// 		this.s3ClientMock.on(CopyObjectCommand).resolves(s3Params);
+			this.s3ClientMock.on(CopyObjectCommand).resolves(response);
 
-	// 		const copyObjectResponse = await S3.copyObject(s3Params);
+			const copyObjectResponse = await S3.copyObject(s3Params);
 
-	// 		assert.deepStrictEqual(copyObjectResponse, response);
-	// 	});
+			assert.deepStrictEqual(copyObjectResponse, response);
+		});
 
-	// 	it('Should rejects the promise calling to copyObject method', async () => {
+		it('Should rejects the promise calling to copyObject method', async () => {
 
+			this.s3ClientMock.on(CopyObjectCommand).resolves(new Error(message));
 
-	// 		this.s3ClientMock.on(PutObjectCommand).resolves(new Error(message));
+			assert.rejects(S3.copyObject(s3Params), {
+				name: 'Error',
+				message
+			});
+		});
 
-	// 		assert.rejects(S3.copyObject(s3Params), {
-	// 			name: 'Error',
-	// 			message
-	// 		});
-	// 	});
+		it('Should call with the same params to copyObject method', async () => {
 
-	// 	// it('Should call with the same params to copyObject method', async () => {
+			this.s3ClientMock.on(CopyObjectCommand).resolves(s3Params);
 
-	// 	// 	await S3.copyObject(s3Params);
+			await S3.copyObject(s3Params);
 
-	// 		// sinon.assert.calledWithExactly(s3Wrapper.copyObject, s3Params);
-	// 	// });
+			this.s3ClientMock.commandCalls(CopyObjectCommand, s3Params);
+		});
 
-	// });
-
-	// context('createPresignedPost', () => {
-
-	// 	const response = {
-	// 		url: 'https://examplebucket.s3.us-east-1.amazonaws.com/',
-	// 		fields: {
-	// 			bucket: 'examplebucket',
-	// 			'X-Amz-Algorithm': 'AWS4-HMAC-SHA256',
-	// 			'X-Amz-Credential': 'AKIAQMPR4JN2JZLNHRH4/20230217/us-east-1/s3/aws4_request',
-	// 			'X-Amz-Date': '20230217T204948Z',
-	// 			key: 'objectkey',
-	// eslint-disable-next-line max-len, max-len
-	// 			Policy: 'eyJleHBpcmF0aW9uIjoiMjAyMy0wMi0xN1QyMTo0OTo0OFoiLCJjb25kaXRpb25zIjpbeyJidWNrZXQiOiJleGFtcGxlYnVja2V0In0seyJYLUFtei1BbGdvcml0aG0iOiJBV1M0LUhNQUMtU0hBMjU2In0seyJYLUFtei1DcmVkZW50aWFsIjoiQUtJQVFNUFI0Sk4ySlpMTkhSSDQvMjAyMzAyMTcvdXMtZWFzdC0xL3MzL2F3czRfcmVxdWVzdCJ9LHsiWC1BbXotRGF0ZSI6IjIwMjMwMjE3VDIwNDk0OFoifSx7ImtleSI6Im9iamVjdGtleSJ9XX0=',
-	// 			'X-Amz-Signature': 'e6dd2d6bb7056dac00aab1fb31341f4006c602feae02f17ddabcb22317464a29'
-	// 		}
-	// 	};
-
-	// 	it('Should return a the same response when calling to createPresignedPost method', async () => {
-
-	// 		sinon.stub(s3Wrapper, 'createPresignedPost');
-
-	// 		s3Wrapper.createPresignedPost.callsFake((params, callback) => {
-	// 			callback(null, response);
-	// 		});
-
-	// 		const createPresignedPostResponse = await S3.createPresignedPost(s3Params);
-	// 		console.log('createPresignedPostResponse:', createPresignedPostResponse);
-	// 		assert.deepStrictEqual(createPresignedPostResponse, response);
-	// 	});
-
-	// 	it('Should rejects the promise calling to createPresignedPost method', async () => {
-
-	// 		sinon.stub(s3Wrapper, 'createPresignedPost');
-
-	// 		s3Wrapper.createPresignedPost.callsFake((params, callback) => {
-	// 			callback(new Error(message), null);
-	// 		});
-
-	// 		assert.rejects(S3.createPresignedPost(s3Params), {
-	// 			name: 'Error',
-	// 			message
-	// 		});
-	// 	});
-
-	// 	// it('Should call with the same params to createPresignedPost method', async () => {
-
-	// 	// 	sinon.stub(s3Wrapper, 'createPresignedPost');
-
-	// 	// 	s3Wrapper.createPresignedPost.callsFake((params, callback) => {
-	// 	// 		console.log('createPresignedPost params:', params);
-	// 	// 		callback(params, response);
-	// 	// 	});
-
-	// 	// 	await S3.createPresignedPost(s3Params);
-	// 	// 	console.log('s3Wrapper.createPresignedPost.getCall(0):', s3Wrapper.createPresignedPost);
-	// 	// 	assert.deepStrictEqual(s3Wrapper.createPresignedPost.getCall(0).args, s3Params);
-	// 	// });
-	// });
-
-	// context('uploadStream', () => {
-
-	// 	it('Should throw an error when s3 upload return an error', async () => {
-
-	// 		const errorMessage = 'Cannot found any bucket with the provided key';
-
-	// 		sinon.stub(S3Client, 'send').returns();
-
-	// 		s3Wrapper.upload.callsFake((params, options, callback) => {
-	// 			callback(new Error(errorMessage), null);
-	// 		});
-
-	// 		const testStream = new PassThrough();
-
-	// 		assert.rejects(S3.uploadStream(testStream, s3Params), {
-	// 			name: 'Error',
-	// 			errorMessage
-	// 		});
-	// 	});
-
-	// 	it('Should return the uploaded object info', async () => {
-
-	// 		const response = {
-	// 			ETag: '"a30c37a2ccde6cf699f557353762815b"',
-	// 			Location: 'https://test.s3.amazonaws.com/test.csv',
-	// 			key: 'test.csv',
-	// 			Key: 'test.csv',
-	// 			Bucket: 'test'
-	// 		};
-
-	// 		sinon.stub(S3Client, 'send').returns();
-
-	// 		s3Wrapper.upload.callsFake((params, options, callback) => {
-	// 			callback(null, response);
-	// 		});
-
-	// 		const testStream = new PassThrough();
-
-	// 		assert.deepStrictEqual(await S3.uploadStream(testStream, s3Params), response);
-	// 	});
-
-	// });
-
-	// context('GetObjectStream', () => {
-
-	// 	it('Should return instance of class get object stream', () => {
-	// 		const testProto = new S3.GetObjectStream();
-	// 		assert.ok(GetObjectStream.prototype.isPrototypeOf(testProto));
-	// 	});
-
-	// 	it('Should process stream chunks and returned', async () => {
-
-	// 		const streamRows = [{ test01: 'test' }, { test02: 'test' }];
-	// 		const testStream = Readable.from(streamRows);
-
-	// 		sinon.stub(S3Client, 'send').returns({ createReadStream: () => testStream });
-
-	// 		const getObjectStream = new S3.GetObjectStream();
-
-	// 		const streamResult = await getObjectStream.call(s3Params);
-
-	// 		const streamData = [];
-	// 		for await (const chunk of streamResult)
-	// 			streamData.push(JSON.parse(chunk.toString()));
-
-	// 		assert.deepStrictEqual(streamData, streamRows);
-	// 	});
-
-	// 	it('Should process stream chunks and returned', async () => {
-
-	// 		const streamRows = [{ test01: 'test' }, { test02: 'test' }];
-	// 		const testStream = Readable.from(streamRows);
-
-	// 		sinon.stub(S3Client, 'getObject').returns({ createReadStream: () => testStream });
-
-	// 		const getObjectStream = new S3.GetObjectStream();
-
-	// 		const streamResult = await getObjectStream.call(s3Params);
-
-	// 		const streamData = [];
-	// 		for await (const chunk of streamResult)
-	// 			streamData.push(JSON.parse(chunk.toString()));
-
-	// 		assert.deepStrictEqual(streamData, streamRows);
-	// 	});
-
-	// 	it('Should process stream chunks with defined parser, process buffer and buffer size', async () => {
-
-	// 		const streamRows = ['test-row-1', 'test-row-2'];
-	// 		const testStream = Readable.from(streamRows);
-
-	// 		sinon.stub(S3Client, 'send').returns({ createReadStream: () => testStream });
-
-	// 		const myGetObjectStream = new MyGetObjectStream();
-
-	// 		const streamResult = await myGetObjectStream.call(s3Params);
-
-	// 		const streamData = [];
-	// 		for await (const chunk of streamResult)
-	// 			streamData.push(chunk.toString());
-
-	// 		assert.deepStrictEqual(streamData, streamRows.map(row => `${row}-processed`));
-	// 	});
-
-	// });
-
+	});
 });
 
 describe('getSignedUrl', () => {
@@ -563,4 +393,152 @@ describe('getSignedUrl', () => {
 
 		// sinon.assert.calledWithExactly(s3Wrapper.getSignedUrl, S3Client, new GetObjectCommand(s3Params), {});
 	});
+});
+
+context('createPresignedPost', () => {
+
+	afterEach(() => sinon.restore());
+
+	const response = {
+		url: 'https://examplebucket.s3.us-east-1.amazonaws.com/',
+		fields: {
+			bucket: 'examplebucket',
+			'X-Amz-Algorithm': 'AWS4-HMAC-SHA256',
+			'X-Amz-Credential': 'AKIAQMPR4JN2JZLNHRH4/20230217/us-east-1/s3/aws4_request',
+			'X-Amz-Date': '20230217T204948Z',
+			key: 'objectkey',
+			// eslint-disable-next-line max-len, max-len
+			Policy: 'eyJleHBpcmF0aW9uIjoiMjAyMy0wMi0xN1QyMTo0OTo0OFoiLCJjb25kaXRpb25zIjpbeyJidWNrZXQiOiJleGFtcGxlYnVja2V0In0seyJYLUFtei1BbGdvcml0aG0iOiJBV1M0LUhNQUMtU0hBMjU2In0seyJYLUFtei1DcmVkZW50aWFsIjoiQUtJQVFNUFI0Sk4ySlpMTkhSSDQvMjAyMzAyMTcvdXMtZWFzdC0xL3MzL2F3czRfcmVxdWVzdCJ9LHsiWC1BbXotRGF0ZSI6IjIwMjMwMjE3VDIwNDk0OFoifSx7ImtleSI6Im9iamVjdGtleSJ9XX0=',
+			'X-Amz-Signature': 'e6dd2d6bb7056dac00aab1fb31341f4006c602feae02f17ddabcb22317464a29'
+		}
+	};
+
+	it('Should return a the same response when calling to createPresignedPost method', async () => {
+
+		sinon.stub(s3Wrapper, 'createPresignedPost').returns(response);
+
+		const createPresignedPostResponse = await S3.createPresignedPost(s3Params);
+
+		assert.deepStrictEqual(createPresignedPostResponse, response);
+	});
+
+	it('Should rejects the promise calling to createPresignedPost method', async () => {
+
+		sinon.stub(s3Wrapper, 'createPresignedPost').returns({
+			name: 'Error',
+			message
+		});
+
+		assert.rejects(S3.createPresignedPost(s3Params), {
+			name: 'Error',
+			message
+		});
+	});
+
+	it('Should call with the same params to createPresignedPost method', async () => {
+
+		sinon.stub(s3Wrapper, 'createPresignedPost').returns(response);
+
+		await S3.createPresignedPost(s3Params);
+
+		assert.deepStrictEqual(s3Wrapper.createPresignedPost.getCall(0).args[1], s3Params);
+	});
+});
+
+context('uploadStream', () => {
+
+	beforeEach(() => {
+		this.s3ClientMock = mockClient(S3Client);
+		this.s3ClientMock.on(CreateMultipartUploadCommand).resolves({ UploadId: '1' });
+		this.s3ClientMock.on(UploadPartCommand).resolves({ ETag: '1' });
+	});
+
+	afterEach(() => {
+		this.s3ClientMock.reset();
+		sinon.restore();
+	});
+
+	it('Should throw an error when s3 upload return an error', async () => {
+
+		const errorMessage = 'Cannot found any bucket with the provided key';
+
+		sinon.stub(s3Wrapper, 'Upload').rejects(new Error(errorMessage));
+
+		const testStream = new PassThrough();
+
+		assert.rejects(S3.uploadStream(testStream, s3Params), {
+			name: 'Error',
+			errorMessage
+		});
+	});
+
+	it('Should return the uploaded object info', async () => {
+
+		const response = {
+			ETag: '"a30c37a2ccde6cf699f557353762815b"',
+			Location: 'https://test.s3.amazonaws.com/test.csv',
+			key: 'test.csv',
+			Key: 'test.csv',
+			Bucket: 'test'
+		};
+
+		sinon.stub(s3Wrapper, 'Upload').returns(response);
+
+		const testStream = new PassThrough();
+
+		assert.deepStrictEqual(await S3.uploadStream(testStream, s3Params), response);
+	});
+});
+
+context('GetObjectStream', () => {
+
+	beforeEach(() => {
+		this.s3ClientMock = mockClient(S3Client);
+	});
+
+	afterEach(() => this.s3ClientMock.reset());
+
+	it.skip('Should return instance of class get object stream', () => {
+		const testProto = new S3.GetObjectStream();
+		assert.ok(GetObjectStream.prototype.isPrototypeOf(testProto));
+	});
+
+	it.skip('Should process stream chunks and returned', async () => {
+
+		const streamRows = [{ test01: 'test' }, { test02: 'test' }];
+		const testStream = Readable.from(streamRows);
+
+		this.s3ClientMock.on(GetObjectCommand).resolves({ createReadStream: () => testStream });
+
+		const getObjectStream = new S3.GetObjectStream();
+
+		const streamResult = await getObjectStream.call(s3Params);
+
+		const streamData = [];
+
+		for await (const chunk of streamResult)
+			streamData.push(JSON.parse(chunk.toString()));
+
+		assert.deepStrictEqual(streamData, streamRows);
+	});
+
+	it.skip('Should process stream chunks with defined parser, process buffer and buffer size', async () => {
+
+		const streamRows = ['test-row-1', 'test-row-2'];
+		const testStream = Readable.from(streamRows);
+
+		this.s3ClientMock.on(GetObjectCommand).resolves({ createReadStream: () => testStream });
+		// sinon.stub(S3Client, 'send').returns({ createReadStream: () => testStream });
+
+		const myGetObjectStream = new MyGetObjectStream();
+
+		const streamResult = await myGetObjectStream.call(s3Params);
+
+		const streamData = [];
+		for await (const chunk of streamResult)
+			streamData.push(chunk.toString());
+
+		assert.deepStrictEqual(streamData, streamRows.map(row => `${row}-processed`));
+	});
+
 });
